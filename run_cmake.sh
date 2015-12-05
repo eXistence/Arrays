6d875a0dffdc5002367d050d9eb6799c38bcb2a0
Johannes Ohlemacher <johannes.ohlemacher@googlemail.com>
2015-12-05 16:11:42 +0100

    reworked the way different build configurations for unit test and benchmark are handled

--- run_cmake.sh
+++ run_cmake.sh
@@ -1,57 +1,55 @@
 #!/bin/sh
 
 BUILD_DIR=build/gcc/debug
-COMPILER="gcc"
-BUILDSYSTEM="make"
+DIRECTORY="gcc"
 BUILDTYPE="debug"
-
 CMAKE_BUILD_TYPE="Debug"
 CMAKE_GENERATOR="Sublime Text 2 - Unix Makefiles"
 
-if [ "$#" -ne "3" -a "$#" -ne "2" ]; then
-	echo "Usage run_cmake.sh <gcc|clang> <debug|release> <libstdc++|libc++>"
-	exit 1
-fi	
+if [ "$#" -ne "2" ]; then
+  echo "Usage run_cmake.sh <gcc|clang|clang-libc++> <debug|release>"
+  exit 1
+fi  
 
-if [ "$#" -ge "1" ]; then	
+if [ "$#" -ge "1" ]; then 
 
-	if [ "$1" = "gcc" ]; then
-		export CXX="g++"
-		export CC="gcc"
-		COMPILER="gcc"
-	fi
+  if [ "$1" = "gcc" ]; then
+    export CXX="g++"
+    export CC="gcc"
+    DIRECTORY="gcc"
+  fi
 
-	if [ "$1" = "clang" ]; then
-		export CXX="clang++"
-		export CC="clang"		
-		COMPILER="clang"
-	fi	
+  if [ "$1" = "clang" ]; then
+    export CXX="clang++"
+    export CC="clang"   
+    DIRECTORY="clang"
+    CMAKE_CXX_FLAGS="-stdlib=libstdc++"
+  fi 
+
+  if [ "$1" = "clang-libc++" ]; then
+    export CXX="clang++"
+    export CC="clang"   
+    DIRECTORY="clang-libc++"
+    CMAKE_CXX_FLAGS="-stdlib=libc++"
+  fi  
 fi
 
-if [ "$#" -ge "2" ]; then	
+if [ "$#" -ge "2" ]; then 
 
-	if [ "$2" = "debug" ]; then
-		BUILDTYPE="debug"
-		CMAKE_BUILD_TYPE="Debug"
-	fi
+  if [ "$2" = "debug" ]; then
+    BUILDTYPE="debug"
+    CMAKE_BUILD_TYPE="Debug"
+  fi
 
-	if [ "$2" = "release" ]; then
-		BUILDTYPE="release"
-		CMAKE_BUILD_TYPE="Release"
-	fi	
+  if [ "$2" = "release" ]; then
+    BUILDTYPE="release"
+    CMAKE_BUILD_TYPE="Release"
+  fi  
 fi
 
-BUILDDIR=build/$COMPILER/$BUILDTYPE
-ROOTDIR=../../..
-
-if [ "$1" = "clang" -a "$#" -ge "3" ]; then	
-	echo "lets use clang with "$3
-	CXX_FLAGS=-DCMAKE_CXX_FLAGS="-stdlib=$3"
-	BUILDDIR=$BUILDDIR/$3
-	ROOTDIR=../../../..
-fi
+BUILDDIR=build/$DIRECTORY/$BUILDTYPE
 
 mkdir -p $BUILDDIR
 cd $BUILDDIR
 
-cmake -G"$CMAKE_GENERATOR" -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE $CXX_FLAGS $ROOTDIR
+cmake -G"$CMAKE_GENERATOR" -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" ../../..
